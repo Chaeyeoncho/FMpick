74426c11e59e23058186688aab7231c78de2911f
Chaeyeoncho <gcy9293@naver.com>
2024-11-25 02:27:51 +0900

    fix : css, js, html 분리

---
+++ product/portato.js
@@ -0,0 +1,16 @@
+// Description: Product page for portato
+        function goBack() {
+            window.history.back();
+        }
+
+        function goHome() {
+            window.location.href = "/index.html";
+        }
+
+        function addToCart() {
+            alert("장바구니에 추가되었습니다!");
+        }
+
+        function buyNow() {
+            alert("구매 페이지로 이동합니다!");
+        }
