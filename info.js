fad76a8c498375604bc2618a81822cbd422052b5
Chaeyeoncho <gcy9293@naver.com>
2024-12-02 20:19:38 +0900

    style : 상품 등록 페이지 스타일 변경

--- info.js
+++ info.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const fileInput = document.getElementById('fileUpload');
     const previewImage = document.getElementById('previewImage');
+    const defaultLabel = document.querySelector('label[for="fileUpload"]'); 
     const submitBtn = document.getElementById('submitBtn');
 
     fileInput.addEventListener('change', function (event) {
@@ -10,35 +11,34 @@ document.addEventListener('DOMContentLoaded', function () {
             reader.onload = function (e) {
                 previewImage.src = e.target.result;
                 previewImage.style.display = 'block';
+
+                if (defaultLabel) {
+                    defaultLabel.style.display = 'none'; 
+                }
             };
             reader.readAsDataURL(file);
         }
     });
 
-
     submitBtn.addEventListener('click', function (event) {
         event.preventDefault();
 
         const productName = document.querySelector('input[placeholder="상품명을 입력하세요"]').value.trim();
-        const category = document.querySelector('input[placeholder="카테고리를 입력하세요"]').value.trim();
         const weight = document.querySelector('input[placeholder="상품 상태를 입력하세요"]').value.trim();
         const description = document.querySelector('textarea[placeholder="상품 설명을 입력하세요"]').value.trim();
-        const tag = document.querySelector('input[placeholder="#태그를 입력하세요"]').value.trim();
         const price = document.querySelector('input[placeholder="가격을 입력하세요"]').value.trim();
-        const image = previewImage.src || '../Img/fmpick_white.svg'; 
+        const image = previewImage.src || '../Img/fmpick_white.svg';
 
-        if (!productName || !category || !weight|| !description || !tag || !price) {
+        if (!productName || !weight || !description || !price) {
             alert("모든 항목을 입력해주세요!");
             return;
         }
 
         const productData = {
-            id: Date.now().toString(), 
+            id: Date.now().toString(),
             name: productName,
-            category,
             weight,
             description,
-            tag,
             price,
             image,
         };
@@ -50,7 +50,18 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem('products', JSON.stringify(existingProducts));
 
         alert('상품이 성공적으로 등록되었습니다!');
-        window.location.href = "home.html"; 
-        
+        window.location.href = "home.html";
+
+
     });
+
 });
+
+function goBack() {
+    window.history.back();
+}
+
+function goHome() {
+    window.location.href = "/FMpick/home.html";
+}
+
